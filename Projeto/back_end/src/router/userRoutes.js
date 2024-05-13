8ebab9a9b11749c2922fc4ae8c2da61c206be701
Mayrton20 <mayrtontrabalho@gmail.com>
2024-05-13 20:08:57 -0300

    update

--- Projeto/back_end/src/router/userRoutes.js
+++ Projeto/back_end/src/router/userRoutes.js
@@ -8,9 +8,11 @@ router.post("/", async (req, res) => {
     const user = req.body;
     console.log(user)
     try {
-        const result = await userDB.insert(user);
+        const [result] = await userDB.insert(user);
+        const insertedUserId = result.insertId;
+        const insertedUser = await userDB.select(insertedUserId);
         res.status(201).json({
-            message: "Cadastro realizado com sucesso!"
+            message: insertedUser
         });
     } catch(err) {
         console.log(err);
@@ -33,6 +35,23 @@ router.get("/", async (req, res) => {
             .json({message : "Ocorreu um erro ao consultar"})
     }
 })
+
+router.get("/:telefone/:id", async (req, res) => {
+    const telefone = req.params.telefone
+    const id = req.params.id
+    try {
+        const result = await userDB.selectTelefone(telefone, id);
+        res.status(200).json({
+            result
+        });
+    } catch (erro) {
+        console.log(erro);
+        res
+            .status(500)
+            .json({message : "Ocorreu um erro ao consultar"})
+    }
+})
+
 
 router.delete("/", async (req, res) => {
     const user = req.query;
